04cc0708f0b0ee107bcb632f9fb3c69399e32bb9
Rodrigo Zanotta <rodrigozanottarsz@gmail.com>
2022-12-01 09:04:57 -0300

    Revert "Revert "Módulo de tipos de produtos""
    This reverts commit 933ee40ff78d349e632f5a9577b84a16e643f20e.

---
+++ application/views/dashboard/tipos_produtos/add.php
@@ -0,0 +1,39 @@
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2"><i class="fas fa-laptop-code"></i> <?php echo $titulo_pagina ?></h1>
+    </div>
+
+    <section id="error-area">
+        <div class="row">
+            <div class="col-12 col-sm-12">
+                <?= validation_errors('<div class="alert alert-danger" role="alert">', '</div>') ?>
+                <?= $this->session->userdata('msg'); ?>
+            </div>
+        </div>
+    </section>
+
+    <div class="row">
+        <div class="col-12 col-sm-12 mb-4">
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label for="nome" class="form-label">Nome</label>
+                            <input type="text" class="form-control" id="nome" name="nome">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label for="slug" class="form-label">Slug</label>
+                            <input type="text" class="form-control" id="slug" name="slug">
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-success mt-3 mb-3 w-100">Adicionar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</main>
